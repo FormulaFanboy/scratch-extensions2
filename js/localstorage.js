17c1e5acc6e88613956deb6ab7e05da4eec679f3
FormulaFanboy <65199262+FormulaFanboy@users.noreply.github.com>
2020-05-11 20:28:02 -0400

    Update localstorage.js

--- js/localstorage.js
+++ js/localstorage.js
@@ -1,31 +1,37 @@
-new (function() {
-	var ext = this;
-	var descriptor = {
-		blocks: [
-			['r', '%s', 'readLocalStorage', 'highscore'],
-			[' ', 'set %s to %s', 'setLocalStorage', 'highscore', '12'],
-		],
-		url : 'https://jgames101.github.io/scratch-extensions/localstorage.html'
-	};
-	ext._shutdown = function() {
-		
-	};
-	
-	ext._getStatus = function() {
-    if (window.localStorage) {
-		  return {status:2, msg:'LocalStorage supported by your browser!'};
-    } else {
-      return {status:0, msg:'LocalStorage is not supported by your browser. Try recent versions of Chrome or Firefox.'};
+new(function() {
+    var ext = this;
+    var descriptor = {
+        blocks: [
+            ['r', '%s', 'readLocalStorage', 'highscore'],
+            [' ', 'set %s to %s', 'setLocalStorage', 'highscore', '12'],
+        ],
+        url: 'https://jgames101.github.io/scratch-extensions/localstorage.html'
     };
-	};
-  
-	ext.readLocalStorage = function(name) {
-		return localStorage.getItem(name);
-	};
-	
-	ext.setLocalStorage = function(name, value) {
-		return localStorage.setItem(name, value);
-	};
+    ext._shutdown = function() {
 
-	ScratchExtensions.register('LocalStorage Variables', descriptor, ext);
+    };
+
+    ext._getStatus = function() {
+        if (window.localStorage) {
+            return {
+                status: 2,
+                msg: 'LocalStorage supported by your browser!'
+            };
+        } else {
+            return {
+                status: 0,
+                msg: 'LocalStorage is not supported by your browser. Try recent versions of Chrome or Firefox.'
+            };
+        };
+    };
+
+    ext.readLocalStorage = function(name) {
+        return localStorage.getItem(name);
+    };
+
+    ext.setLocalStorage = function(name, value) {
+        return localStorage.setItem(name, value);
+    };
+
+    ScratchExtensions.register('LocalStorage Variables', descriptor, ext);
 })();
